14233addbf666e82f2030f6d15c15c4ba2defbcb
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-25 11:58:08 +0200

    Escribir y leer Cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, effect, Inject, inject, Optional } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'i18n-app';
+
+  cookie = inject(SsrCookieService);
+
+  cookieLogEffect = effect(() => {
+    console.log({ cookie: this.cookie.get('lang') });
+  });
 }
