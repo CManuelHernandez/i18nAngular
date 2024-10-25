55625d774d7f3a437b1375a70549a11eec7f9f81
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-25 12:18:11 +0200

    Archivos de traducciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,11 @@ export class AppComponent {
   cookie = inject(SsrCookieService);
   languageService = inject(LanguageService);
 
-  cookieLogEffect = effect(() => {
+  constructor() {
     console.log({ cookie: this.cookie.get('lang') });
 
     const lang = this.cookie.check('lang') ? this.cookie.get('lang') : 'en';
 
     this.languageService.changeLang(lang);
-  });
+  }
 }
